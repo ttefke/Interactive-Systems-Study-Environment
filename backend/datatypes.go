1e85314418109cb8f8ea61aa5b226d0922e068a6
Tobias Tefke <tobias@tefke.name>
2023-12-10 15:07:07 +0100

    backend: update gomodules and allow tracking how many letters were input into enhanced form

--- backend/datatypes.go
+++ backend/datatypes.go
@@ -5,6 +5,7 @@ type Data struct {
 	UserId               int64 `json:"userId"`
 	DefaultRequiredTime  int64 `json:"defaultRequiredTime"`
 	DefaultAttempts      int64 `json:"defaultAttempts"`
+	EnhancedLetters      int64 `json:"enhancedLetters"`
 	EnhancedRequiredTime int64 `json:"enhancedRequiredTime"`
 	EnhancedAttempts     int64 `json:"enhancedAttempts"`
 }
